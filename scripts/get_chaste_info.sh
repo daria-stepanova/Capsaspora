7faf12fd44945f443b4bd36586dd16fcaf210e01
daria-stepanova <daria.stepanova.zavalei@gmail.com>
2025-02-25 14:25:22 +0100

    Scripts folder

---
+++ scripts/get_chaste_info.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ -z "$CHASTE_DIR" ]; then
+    export CHASTE_DIR="/path/to/Chaste"
+fi
+if [ -z "$CHASTE_BUILD_DIR" ]; then
+    export CHASTE_BUILD_DIR="${CHASTE_DIR}/build"
+fi
+
+( cd $CHASTE_BUILD_DIR && ctest --verbose -R TestChasteBuildInfo$ )
